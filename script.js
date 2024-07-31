bedd5fd3e263bed1d7ac90c1ede6c38b6e8cb7e0
mathiassubtilaluno <103583059+mathiassubtilaluno@users.noreply.github.com>
2024-07-31 08:27:48 -0300

    Atividade

---
+++ script.js
@@ -0,0 +1,8 @@
+//Mensagem de boas-vindas ao clicar no botão
+document.addEventListener('DOMContentLoaded;()=>
+  const button=document.querySelector('click', ()=>
+alert(Estamos animados para começar o ano com vocês!');
+));
+
+
+//Contador de dias para o início das aulas
